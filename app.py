af77b57901f954d00664d1c1c6408f2592669800
sangjw-custom <cantabile05@gmail.com>
2026-04-08 14:36:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -194,6 +194,9 @@ elif menu == "📋 통합 거래 이력":
     if not log_df.empty:
         log_df['sort_date'] = pd.to_datetime(log_df['입력일자'])
         display_log = log_df.sort_values("sort_date", ascending=False).drop(columns=['sort_date'])
+
+        # 열 순서 (프로젝트 정보 전면 배치)
+        ordered_cols = ["유형" "상태" "프로젝트코드", "프로젝트명", "상품유형", "상품코드", "상품명", "단위", "수량", "단가", "총액", "문서번호", "입력일자", "입력자", "확정일자"]
         
         # 숫자 컬럼들에 대해 콤마 포맷 적용
         formatted_log = display_log.style.format({
